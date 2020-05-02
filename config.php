ace044e64cd55ede4e7219bac9c5569c0c431ed8
saltrupiao <strupiano@oakland.edu>
2020-05-02 14:14:59 -0400

    Added initial login functionality

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: saltrupiano
+ * Date: 5/2/20
+ * Time: 2:11 PM
+ * Source: https://www.tutorialrepublic.com/php-tutorial/php-mysql-login-system.php
+ */
+
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'oakland');
+define('DB_NAME', 'timetracker');
+
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?>
